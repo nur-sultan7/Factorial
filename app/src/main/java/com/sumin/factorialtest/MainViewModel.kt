27e9d4f87805581e9b1f045bf5f7d7e0ff890c9c
Nursultan Akhmadov <nur-sultan17@mail.ru>
2022-05-05 18:51:16 +0300

    impl own coroutine scope with coroutineContext

--- app/src/main/java/com/sumin/factorialtest/MainViewModel.kt
+++ app/src/main/java/com/sumin/factorialtest/MainViewModel.kt
@@ -4,12 +4,13 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import java.math.BigInteger
+import kotlin.math.floor
 
 class MainViewModel : ViewModel() {
+    private val coroutineScope =
+        CoroutineScope(Dispatchers.Main + CoroutineName("My coroutine scope"))
     private val _state = MutableLiveData<State>()
     val state: LiveData<State>
         get() = _state
@@ -20,31 +21,41 @@ class MainViewModel : ViewModel() {
             _state.value = Error
             return
         }
+        val scope = coroutineScope.async {
+            withContext(Dispatchers.Default) {
+                getFactorial(value.toLong())
+            }
+        }
         viewModelScope.launch {
-            val factorial = getFactorial(value.toLong())
-            _state.value = Factorial(factorial)
+            _state.value = Factorial(scope.await())
         }
     }
 
-    private suspend fun getFactorial(value: Long): String {
-        return withContext(Dispatchers.Default) {
-            var resultString = "Unknown"
-            var result = BigInteger.ONE
-            val percentOfValue = value / 10
-            var progressTrigger = percentOfValue
-            for (i in 1..value) {
-                result = result.multiply(BigInteger.valueOf(i))
-                if (i == progressTrigger) {
-                    if (i == value)
-                        resultString = result.toString()
-                    _state.postValue(Progress(i.toInt()))
-                    progressTrigger += percentOfValue
-                }
+    private fun getFactorial(value: Long): String {
+        var resultString = "Unknown"
+        var result = BigInteger.ONE
+        val percentOfValue = value / 10f
+        var progressTrigger = percentOfValue
+        for (i in 1..value) {
+            result = result.multiply(BigInteger.valueOf(i))
+            if (i == value) {
+                resultString = result.toString()
             }
-            resultString
+            if (i >= progressTrigger) {
+                _state.postValue(Progress(i.toInt()))
+                progressTrigger += percentOfValue
+                progressTrigger = floor(progressTrigger * 10) / 10f
+            }
         }
+        return resultString
     }
-//    private suspend fun getFactorial(value: Long): String {
+
+    override fun onCleared() {
+        super.onCleared()
+        coroutineScope.cancel()
+    }
+
+    //    private suspend fun getFactorial(value: Long): String {
 //        return suspendCoroutine {
 //            thread {
 //                var resultString = "Unknown"
